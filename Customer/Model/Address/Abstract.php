b5fee2e8b087ef19a72998aea329faa422a99299
Cosmo Mao <gongyuan.mao@cosmocommerce.com>
2013-11-17 21:35:11 +0800

    add address rewrite

---
+++ Customer/Model/Address/Abstract.php
@@ -0,0 +1,26 @@
+<?php
+class CosmoCommerce_Customer_Model_Address_Abstract extends Mage_Customer_Model_Address_Abstract
+{
+    /**
+     * Get full customer name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        $name = '';
+        $config = Mage::getSingleton('eav/config');
+        if ($config->getAttribute('customer_address', 'prefix')->getIsVisible() && $this->getPrefix()) {
+            $name .= $this->getPrefix() . ' ';
+        }
+        $name .=  . $this->getLastname();
+        if ($config->getAttribute('customer_address', 'middlename')->getIsVisible() && $this->getMiddlename()) {
+            $name .= ' ' . $this->getMiddlename();
+        }
+        $name .= $this->getFirstname();
+        if ($config->getAttribute('customer_address', 'suffix')->getIsVisible() && $this->getSuffix()) {
+            $name .= ' ' . $this->getSuffix();
+        }
+        return $name;
+    }
+}    
