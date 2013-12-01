2f48fd25286e835c0262faab7682049a25be1d98
Cosmo Mao <gongyuan.mao@cosmocommerce.com>
2013-12-01 16:48:16 +0800

    fix customer when save at backend.

--- Customer/Model/Form.php
+++ Customer/Model/Form.php
@@ -5,12 +5,16 @@ class CosmoCommerce_Customer_Model_Form extends Mage_Customer_Model_Form
     public function extractData(Zend_Controller_Request_Http $request, $scope = null, $scopeOnly = true)
     {
         if($postdata=$request->getPost()){
-            $fullname=$postdata['fullname'];
-            if($fullname){
-                $lastname=mb_substr($fullname,0,1,"UTF-8");
-                $firstname=mb_substr($fullname,1,mb_strlen($fullname)-1,"UTF-8");
-                $request->setParam('firstname',$firstname);
-                $request->setParam('lastname',$lastname);
+            if(isset($postdata['fullname'])){
+                $fullname=$postdata['fullname'];
+                if($fullname){
+                    $lastname=mb_substr($fullname,0,1,"UTF-8");
+                    $firstname=mb_substr($fullname,1,mb_strlen($fullname)-1,"UTF-8");
+                    $request->setParam('firstname',$firstname);
+                    $request->setParam('lastname',$lastname);
+                }
+            }else{
+            
             }
         }
         $data = array();
