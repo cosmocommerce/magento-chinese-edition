17818f395b194f4592870720b986166068736cb2
root <root@AY131031223045146507Z.(none)>
2014-05-16 15:25:18 +0800

    add reports module

---
+++ Reports/Model/Resource/Order/Collection.php
@@ -0,0 +1,18 @@
+<?php
+class CosmoCommerce_Reports_Model_Resource_Order_Collection extends Mage_Reports_Model_Resource_Order_Collection
+{
+    /**
+     * Join Customer Name (concat)
+     *
+     * @param string $alias
+     * @return Mage_Reports_Model_Resource_Order_Collection
+     */
+    public function joinCustomerName($alias = 'name')
+    {
+        $fields      = array('main_table.customer_firstname', 'main_table.customer_lastname');
+        $fieldConcat = $this->getConnection()->getConcatSql($fields, ' ');
+        $this->getSelect()->columns(array($alias => $fieldConcat));
+        return $this;
+    }
+
+}
--- Sales/Model/Order.php
+++ Sales/Model/Order.php
@@ -2,6 +2,18 @@
 class CosmoCommerce_Sales_Model_Order extends Mage_Sales_Model_Order
 {
     
+    
+    public function getCustomerName()
+    {
+        if ($this->getCustomerFirstname()) {
+            $customerName = $this->getCustomerLastname() . '' . $this->getCustomerFirstname();
+        }
+        else {
+            $customerName = Mage::helper('sales')->__('Guest');
+        }
+        return $customerName;
+    }
+    
     public function canRepay()
     {
         if($this->getPayment()->getMethod()=="alipay_payment"){
